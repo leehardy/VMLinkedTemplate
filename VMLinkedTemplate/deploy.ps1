f4af8155d417965754807b1b9b0aee773f309f6d
Lee Hardy <leejghardy@gmail.com>
2017-03-09 23:32:45 +1100

    deploy.ps1 now functional
    Generates random 15 character storage account name
    Checks if storage account name exists
    Creates storage account
    Uploads ARM VM linked templates
    Deploys deploy.json template using linked VM templates
    Deletes storage account

--- VMLinkedTemplate/deploy.ps1
+++ VMLinkedTemplate/deploy.ps1
@@ -1,8 +1,7 @@
 $now = Get-Date
 $expiry = (Get-Date).AddHours(2)
-$templateFolder = "C:\Users\lee0592\Documents\personal-git-repos\VMLinkedTemplate\VMLinkedTemplate\VirtualMachines"
+$templateFolder = "C:\Users\Lee Hardy\Documents\git-repos\VMLinkedTemplate\VMLinkedTemplate\VirtualMachines"
 $templates = Get-ChildItem $templateFolder
-$storageAccountName = "depltempsa1993"
 $storageAccountResourceGroup = "storage-prod-rg"
 $containerName = "templates"
 $policyName = "templateDeploymentPolicy"
@@ -13,16 +12,15 @@ function createStorageAccount ($resourceGroup) {
 	Do {
 		$saPrefix = -join ((97..122) | Get-Random -Count 10 | % {[char]$_})
 		$saRandName = $saPrefix + "slrs1"
-		$sa = Get-AzureRmStorageAccount -Name $saRandName -ResourceGroupName $resourceGroup
+		$sa = Get-AzureRmStorageAccount -Name $saRandName -ResourceGroupName $resourceGroup -ErrorAction SilentlyContinue
 	} While ($sa)
 
-	New-AzureRmStorageAccount -Name $sa -ResourceGroupName $resourceGroup -SkuName Standard_LRS -Location australiaeast -Kind Storage
+	New-AzureRmStorageAccount -Name $saRandName -ResourceGroupName $resourceGroup -SkuName Standard_LRS -Location australiaeast -Kind Storage
 }
 
 Login-AzureRmAccount
 
-
-
+$storageAccountName = (createStorageAccount($storageAccountResourceGroup)).StorageAccountName
 Set-AzureRmCurrentStorageAccount -Name $storageAccountName -ResourceGroupName $storageAccountResourceGroup
 
 New-AzureStorageContainer -Name templates
@@ -36,5 +34,5 @@ foreach ($template in $templates) {
 
 New-AzureRmResourceGroupDeployment -Name "NewVM" -ResourceGroupName $deploymentResourceGroup -TemplateFile ".\deploy.json" -TemplateParameterFile ".\deploy.parameters.json" -containerSasToken $sasToken  -storageAccountName $storageAccountName
 
-Remove-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $storageAccountResourceGroup
+Remove-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $storageAccountResourceGroup -Confirm
 
