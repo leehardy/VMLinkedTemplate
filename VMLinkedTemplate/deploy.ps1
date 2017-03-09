1ae75cdfb97f9367a76af55d82b827aa33f02ab0
Lee Hardy <Lee.Hardy@rackspace.com>
2017-03-09 16:58:15 +1100

    Working on Powershell deploy.ps1
    Need to create working function that tests if Storage Account Name exists, before creating the SA. Currently WIP

---
+++ VMLinkedTemplate/deploy.ps1
@@ -0,0 +1,40 @@
+$now = Get-Date
+$expiry = (Get-Date).AddHours(2)
+$templateFolder = "C:\Users\lee0592\Documents\personal-git-repos\VMLinkedTemplate\VMLinkedTemplate\VirtualMachines"
+$templates = Get-ChildItem $templateFolder
+$storageAccountName = "depltempsa1993"
+$storageAccountResourceGroup = "storage-prod-rg"
+$containerName = "templates"
+$policyName = "templateDeploymentPolicy"
+$deploymentResourceGroup = "vm-prod-rg"
+
+
+function createStorageAccount ($resourceGroup) {
+	Do {
+		$saPrefix = -join ((97..122) | Get-Random -Count 10 | % {[char]$_})
+		$saRandName = $saPrefix + "slrs1"
+		$sa = Get-AzureRmStorageAccount -Name $saRandName -ResourceGroupName $resourceGroup
+	} While ($sa)
+
+	New-AzureRmStorageAccount -Name $sa -ResourceGroupName $resourceGroup -SkuName Standard_LRS -Location australiaeast -Kind Storage
+}
+
+Login-AzureRmAccount
+
+
+
+Set-AzureRmCurrentStorageAccount -Name $storageAccountName -ResourceGroupName $storageAccountResourceGroup
+
+New-AzureStorageContainer -Name templates
+New-AzureStorageContainerStoredAccessPolicy -Policy $policyName -Container $containerName -Permission rl -StartTime $now -ExpiryTime $expiry
+$sasToken = New-AzureStorageContainerSASToken -Name $containerName -Policy $policyName
+
+foreach ($template in $templates) {
+	$templateToUpload = "$templateFolder\$template"
+	Set-AzureStorageBlobContent -File $templateToUpload -Container $containerName -Blob $template -Force
+}
+
+New-AzureRmResourceGroupDeployment -Name "NewVM" -ResourceGroupName $deploymentResourceGroup -TemplateFile ".\deploy.json" -TemplateParameterFile ".\deploy.parameters.json" -containerSasToken $sasToken  -storageAccountName $storageAccountName
+
+Remove-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $storageAccountResourceGroup
+
